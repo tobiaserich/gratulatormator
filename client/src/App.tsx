6cc912f609fe6d9dcc85fab844306e51c8efa641
tobiaserich <tobiaserich@hotmail.de>
2020-05-17 12:09:44 +0200

    Add Router and default route to the landingpage to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import { ThemeProvider } from "emotion-theming";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import colors from "./themes/colors";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <>
+    <Router>
       <ThemeProvider theme={colors}>
         <GlobalStyles />
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
       </ThemeProvider>
-    </>
+    </Router>
   );
 }
 
