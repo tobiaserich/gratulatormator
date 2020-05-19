ad1cafedb42995f6b664301789f2ffed16aa08b1
tobiaserich <tobiaserich@hotmail.de>
2020-05-19 23:32:59 +0200

    Add next birthday component to main page

---
+++ client/src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "@emotion/styled";
+import NextBirthday from "../components/NextBirthday";
+const Main = () => {
+  return (
+    <>
+      <NextBirthday />
+    </>
+  );
+};
+
+export default Main;
