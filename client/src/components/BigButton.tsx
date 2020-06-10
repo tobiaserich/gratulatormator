c884c7f8719ab3354bd6f509e2f0ba248f7b4704
tobiaserich <tobiaserich@hotmail.de>
2020-06-10 12:28:13 +0200

    Create a big button component

---
+++ client/src/components/BigButton.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+type ThemeProps = {
+  theme: any;
+  fontSize?: number;
+  fontFamily?: string;
+
+  spacingTop?: number;
+};
+
+const BigButton = styled("button")<ThemeProps>`
+  font-family: ${({ fontFamily = "inherit" }) => fontFamily};
+  font-size: ${({ fontSize }) => fontSize}px};
+  background-color: ${({ theme }) => theme.action200};
+  color:white;
+  border:none;
+  padding:5px 20px 5px 20px;
+  border-radius: 4px;
+   ${({ spacingTop }) => `margin-top: ${spacingTop}px`};
+`;
+
+export default BigButton;
