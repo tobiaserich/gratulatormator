5795a739e2ea8f646c4dc3fa45568aa68f91cb75
tobiaserich <tobiaserich@hotmail.de>
2020-06-14 20:27:39 +0200

    Add new props

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -5,16 +5,22 @@ type ThemeProps = {
   fontSize?: number;
   spacingRight?: number;
   spacingTop?: number;
+  weight?: string;
+  borderColor?: string;
+  buttonWidth?: number;
 };
 
 const Button = styled("button")<ThemeProps>`
   font-family: inherit;
-  font-size: ${({ fontSize }) => fontSize}px};
+  ${({ fontSize }) => (fontSize ? `font-size:${fontSize}px` : "")};
+  ${({ weight }) => (weight ? `font-weight:${weight}` : "")}};
   background-color: transparent;
-  border: 2.3px solid ${({ theme }) => theme.action100};
+  border: 2.3px solid ${({ borderColor, theme }) =>
+    borderColor ? borderColor : theme.action100};
   border-radius: 4px;
-   ${({ spacingRight }) => `margin-right: ${spacingRight}px`};
-   ${({ spacingTop }) => `margin-top: ${spacingTop}px`};
-`;
+  ${({ spacingRight }) => `margin-right: ${spacingRight}px`};
+  ${({ spacingTop }) => `margin-top: ${spacingTop}px`};
+ ${({ buttonWidth }) => (buttonWidth ? `width:${buttonWidth}px` : "")};
+  `;
 
 export default Button;
