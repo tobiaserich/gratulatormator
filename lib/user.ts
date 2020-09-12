ef7b13cc9808d2b757843ca63dda244571b70102
tobiaserich <tobiaserich@hotmail.de>
2020-09-12 12:49:54 +0200

    Add a verify and login user function

--- lib/user.ts
+++ lib/user.ts
@@ -1,6 +1,9 @@
 export {};
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { ObjectID } = require("mongodb");
 const { getCollection } = require("./db");
+require("dotenv").config();
 
 const registerUser = async (data) => {
   const hashedPassword = await bcrypt.hash(data.password, 10);
@@ -26,4 +29,31 @@ const registerUser = async (data) => {
   return inUse;
 };
 
+const loginUser = async (data) => {
+  const collection = getCollection("users");
+  const inputPassword = data.password;
+  const findUser = await collection.findOne({ username: data.username });
+  if (findUser) {
+    const storedPassword = findUser.password;
+    const comparePassword = await bcrypt.compare(inputPassword, storedPassword);
+    if (comparePassword) {
+      const token = jwt.sign({ _id: findUser._id }, process.env.TOKEN_SECRET);
+      return token;
+    } else {
+      return "loginFailed";
+    }
+  } else {
+    return "loginFailed";
+  }
+};
+
+const verifyUser = async (token) => {
+  const collection = getCollection("users");
+  const userID = jwt.verify(token, process.env.TOKEN_SECRET);
+  const user = await collection.findOne({ _id: ObjectID(userID._id) });
+  return user ? true : false;
+};
+
 exports.registerUser = registerUser;
+exports.loginUser = loginUser;
+exports.verifyUser = verifyUser;
