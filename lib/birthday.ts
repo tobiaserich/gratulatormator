cf27d363994323483fa60c7ce98760c580aea095
tobiaserich <tobiaserich@hotmail.de>
2020-09-21 19:23:46 +0200

    Add the api function for get all the birthdays and improve the error handling

--- lib/birthday.ts
+++ lib/birthday.ts
@@ -7,13 +7,25 @@ const addBirthday = async (data, cookie) => {
   const birthdayData = data;
   birthdayData.owner = jwt.verify(cookie, process.env.TOKEN_SECRET)._id;
   const birthdayValidation = await collection.findOne(birthdayData);
-
-  if (!birthdayValidation) {
-    collection.insertOne(birthdayData);
-    return 200;
-  } else {
-    return 409;
+  try {
+    if (!birthdayValidation) {
+      collection.insertOne(birthdayData);
+      return 200;
+    } else {
+      throw 409;
+    }
+  } catch (error) {
+    return error;
   }
 };
 
+const getAllBirthdays = async (cookie) => {
+  const collection = getCollection("birthdays");
+  const birthdayOwner = jwt.verify(cookie, process.env.TOKEN_SECRET)._id;
+  const cursor = await collection.find({ owner: birthdayOwner });
+  const birthdays = await cursor.toArray();
+  return birthdays;
+};
+
 exports.addBirthday = addBirthday;
+exports.getAllBirthdays = getAllBirthdays;
