5636be8489b67acaeef9509b15e3e6cfb21d2e07
tobiaserich <tobiaserich@hotmail.de>
2020-10-11 17:35:46 +0200

    Add new props

--- client/src/components/Info.tsx
+++ client/src/components/Info.tsx
@@ -3,11 +3,15 @@ import styled from "@emotion/styled";
 type infoprops = {
   fontSize?: number;
   topSpacing?: number;
+  status?: string;
+  theme: any;
 };
 
 const Info = styled("div")<infoprops>`
   font-size: ${({ fontSize = 1.6 }) => fontSize}em;
   font-weight: 600;
-  ${({ topSpacing }) => (topSpacing ? `margin-top:${topSpacing}px` : "")}
+  ${({ topSpacing }) => (topSpacing ? `margin-top:${topSpacing}px` : "")};
+  color: ${({ status, theme }) =>
+    status === "disable" ? theme.neutral400 : ""};
 `;
 export default Info;
