f0b4d4a416db34fd53a53a7d7b4eef520201a418
tobiaserich <tobiaserich@hotmail.de>
2020-10-12 14:10:56 +0200

    Fix the name in the delete modal

--- client/src/pages/UserInfo.tsx
+++ client/src/pages/UserInfo.tsx
@@ -84,7 +84,11 @@ const UserInfo = () => {
         </BigButton>
       </MainContainer>
       {deleteUser ? (
-        <DeleteModal handleVisibility={setDeleteUser} id={id} />
+        <DeleteModal
+          handleVisibility={setDeleteUser}
+          birthdayChildName={`${birthdayChildData["firstName"]} ${birthdayChildData["lastName"]}`}
+          id={id}
+        />
       ) : (
         ""
       )}
