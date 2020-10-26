25fc33523e3762b90a4474bbac9aa13193bad16c
tobiaserich <tobiaserich@hotmail.de>
2020-10-26 12:15:27 +0100

    Add proptypes

--- client/src/api/birthdays.ts
+++ client/src/api/birthdays.ts
@@ -1,3 +1,7 @@
+type getBirthdayProps = {
+  [index: string]: string | boolean;
+};
+
 const addBirthday = async (birthdayData: object): Promise<string> => {
   const response = await fetch("/birthday/add/", {
     method: "POST",
@@ -20,7 +24,7 @@ const getAllBirthdays = async (): Promise<object> => {
   return result;
 };
 
-const getBirthday = async (id: string): Promise<object> => {
+const getBirthday = async (id: string): Promise<getBirthdayProps> => {
   const response = await fetch(`/birthday/singleBirthday/${id}`, {
     method: "GET",
     credentials: "include",
